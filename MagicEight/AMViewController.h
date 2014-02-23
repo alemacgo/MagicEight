c8f53fc8bed77d5acc9b088004f3f95627d2ca12
Alejandro Machado <alemacgo@gmail.com>
2014-02-23 18:10:05 +0000

    Make the array a property

--- MagicEight/AMViewController.h
+++ MagicEight/AMViewController.h
@@ -9,7 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface AMViewController : UIViewController
+
 @property (strong, nonatomic) IBOutlet UILabel *predictionLabel;
+@property (strong, nonatomic) NSArray *predictions;
+
 - (IBAction)buttonPressed;
 
 @end
--- MagicEight/AMViewController.m
+++ MagicEight/AMViewController.m
@@ -18,12 +18,24 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.predictions = [[NSArray alloc] initWithObjects:
+       @"It is certain.",
+       @"It is decidedly so.",
+       @"All signs say YES.",
+       @"The stars are\nnot aligned.",
+       @"My reply is NO.",
+       @"It is doubtful.",
+       @"Better not tell you now.",
+       @"Concentrate and ask again.",
+       @"I'm unable to answer now.",
+       nil];
 }
 
 /*
 - (void)viewDidAppear:(BOOL)animated {
     [super viewDidAppear:animated];
-    
+ 
     CGRect frame = self.predictionLabel.frame;
     self.predictionLabel.frame = CGRectMake(frame.origin.x, 200, frame.size.width, frame.size.height);
 }*/
@@ -35,20 +47,8 @@
 }
 
 - (IBAction)buttonPressed {
-    
-    NSArray *predictions = [[NSArray alloc] initWithObjects:
-        @"It is certain.",
-        @"It is decidedly so.",
-        @"All signs say YES.",
-        @"The stars are\nnot aligned.",
-        @"My reply is NO.",
-        @"It is doubtful.",
-        @"Better not tell you now.",
-        @"Concentrate and ask again.",
-        @"I'm unable to answer now.",
-        nil];
-    
-    self.predictionLabel.text = [predictions objectAtIndex:3];
+        
+    self.predictionLabel.text = [self.predictions objectAtIndex:3];
     self.predictionLabel.textColor = [UIColor redColor];
 }
 @end
